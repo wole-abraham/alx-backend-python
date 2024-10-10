6c204b197361e71a87e3f68c216df999caf381c4
abraham <wole1702@gmail.com>
2024-10-11 00:45:22 +0100

    code

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
-""" complex type thats cjecle out finctions """
+""" complex type thats cjecle out finctions
+    ufgiiiiiiiiiiiiiiiiiiiiiiiiii
+"""
 from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
     """ funtion to kv cjecke out
+        hoooooooiiiiiiiiohhhhhhhhhhhhh
     """
 
     return (k, v ** 2)
