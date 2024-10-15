4b17c7d39a994cd6e1b4b30581949a61787fa19b
wole-abraham <123215687+wole-abraham@users.noreply.github.com>
2024-10-15 15:38:01 -0700

    Update 0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 """ Python script using async functions"""
-import asyncio
 import random
+import asyncio
 
-async def random_number_generator():
+
+async def async_generator():
     """
     Coroutine that loops 10 times, each time asynchronously waits for 1 second,
     then yields a random number between 0 and 10.
     """
     for _ in range(10):
         await asyncio.sleep(1)  # Asynchronously wait for 1 second
-        yield random.randint(0, 10)  # Yield a random integer between 0 and 10
+        yield random.uniform(0, 10)  # Yield a random float between 0
