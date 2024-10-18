151c021de392e2d2d7c0009b5151d0b85e103964
wole-abraham <123215687+wole-abraham@users.noreply.github.com>
2024-10-18 22:15:21 +0100

    Create 2-measure_runtime.py

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+pythons script asuncc comprehension
+"""
+
+
+import time
+import asyncio
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Measure the total runtime"""
+    start_time = time.time()
+
+    await asyncio.gather(*(async_comprehension() for i in range(4)))
+
+    end_time = time.time()
+    return end_time - start_time
